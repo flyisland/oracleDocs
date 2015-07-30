f373418dcb3fd244f850ae04c8df7a84d285e702
Island Chen <flyisland@gmail.com>
2015-07-30 22:53:04 +0800

    using slices to store the product lists

--- fmw12c.go
+++ fmw12c.go
@@ -6,17 +6,24 @@ import (
 	"os"
 	"regexp"
 	"strings"
+	"net/url"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-var invalidFileName = []string{"/", "\\", ":", "*", "?", "\"", "<", ">", "|"}
-var fmwBaseURL = "http://docs.oracle.com/en/middleware/"
-var fmwPage = fmwBaseURL + "middleware.html"
-var s12c = ".rel1213" // selector for 1213
+type prodLink struct {
+	abbr	string
+	name	string
+	href	*url.URL
+}
 
-func listAllProducts() map[string]string {
-	doc, err := goquery.NewDocument(fmwPage)
+var invalidFileName = []string{"/", "\\", ":", "*", "?", "\"", "<", ">", "|"}
+var mwURL = "http://docs.oracle.com/en/middleware/middleware.html"
+var versionSelector = ".rel1213" // selector for 1213
+var prodSlices = make([]prodLink, 0, 60)
+
+func listAllProducts() {
+	doc, err := goquery.NewDocument(mwURL)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -26,24 +33,27 @@ func listAllProducts() map[string]string {
 	var title = ""
 	//	"../../middleware/1213/wls/index.html"
 	var re = regexp.MustCompile(`middleware/\d+/(\w+)/\w+.htm`)
-	var mProducts = make(map[string]string)
+	var findedlink	prodLink
 
-	doc.Find(s12c).Find("a").Each(func(i int, s *goquery.Selection) {
+	doc.Find(versionSelector).Find("a").Each(func(i int, s *goquery.Selection) {
 		href, finded = s.Attr("href")
 		title = s.Text()
 		matchs := re.FindStringSubmatch(href)
 
 		if len(matchs) > 1 {
-			mProducts[matchs[1]] = title
+			findedlink.abbr = matchs[1]
+			findedlink.name = title
+			findedlink.href, _ = doc.Url.Parse(href)
+			prodSlices = append(prodSlices, findedlink)
 		}
-
 	})
-
-	mProducts["cross"] = "Common Documents for Fusion Middleware"
-	return mProducts
 }
 
 func buildURLs(product string) {
+	if (product == "cross"){
+		buildCrossURLs()
+		return
+	}
 	var baseURL = "http://docs.oracle.com/middleware/1213/" + product + "/"
 	doc, err := goquery.NewDocument(baseURL + "docs.htm")
 	if err != nil {
@@ -62,8 +72,6 @@ func buildURLs(product string) {
 				// find the booktitle block
 				bookTitle := s.Find(".booktitle").Text()
 				bookTitle = strings.TrimSpace(bookTitle)
-				//				bookTitle = strings.Replace(bookTitle, "Fusion Middleware ", "", 1)
-				//				bookTitle = strings.Replace(bookTitle, "Oracle Fusion Middleware ", "", 1)
 
 				for _, c := range invalidFileName {
 					bookTitle = strings.Replace(bookTitle, c, "_", -1)
@@ -77,16 +85,23 @@ func buildURLs(product string) {
 	})
 }
 
+// Common Documents for Fusion Middleware
+func buildCrossURLs() {
+	// 1. get all urls wiht "cross"
+	// 2. get into each page and find the url for "Books"
+	// 3. search the book page find pdf under "/core/"
+}
+
 func readme() {
 	fmt.Println("Usage: fmw12c PRODUCTNAME")
 	fmt.Println("Build commands to download offline files for this product.")
 	fmt.Println("")
 	fmt.Println("PRODUCTNAME=wls     : download files for WebLogic Server.")
 	fmt.Println("PRODUCTNAME=LIST    : list all products.")
-	fmt.Println("PRODUCTNAME=ALL     : download files for all products!")
 }
 
 func main() {
+
 	if len(os.Args) == 1 {
 		readme()
 		os.Exit(0)
@@ -94,18 +109,12 @@ func main() {
 
 	var product = os.Args[1]
 
-	mProducts := listAllProducts()
+	listAllProducts()
 	switch product {
 	case "LIST":
 		{
-			for p, title := range mProducts {
-				fmt.Printf("%s -> %s\n", p, title)
-			}
-		}
-	case "ALL":
-		{
-			for p := range mProducts {
-				buildURLs(p)
+			for i, s := range prodSlices {
+				fmt.Printf("%02d. %15s -> %s\n", i, s.abbr, s.name)
 			}
 		}
 	default:
